dfd4a952f8d813dac477954fa770b338ee042d6e
IdeasandConcepts <hantoobahmed@gmail.com>
2024-06-11 04:35:22 -0700

    11/june commit

---
+++ lib/controller/localization_and_theme/changelocal_and_theme_controller.dart
@@ -0,0 +1,46 @@
+import '../services/services.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class LocaleAndThemeController extends GetxController {
+  Locale? language;
+  MyServices myServices = Get.find();
+  late ThemeData appTheme;
+
+  changLang(String langCode) {
+    Locale locale = Locale(langCode);
+    // save in shard ...
+    myServices.sharedPreferences.setString("lang", langCode);
+    Get.updateLocale(locale);
+  }
+
+  changeTheme() {
+    if (Get.isDarkMode) {
+      Get.changeTheme(ThemeData.light(useMaterial3: true));
+    } else {
+      Get.changeTheme(ThemeData.dark(useMaterial3: true));
+      // save in shard ...
+    }
+    myServices.sharedPreferences.setBool("isDarkMode", !Get.isDarkMode);
+  }
+
+  @override
+  void onInit() {
+    String? sharedlang = myServices.sharedPreferences.getString("lang");
+    bool? sharedisDarkMode = myServices.sharedPreferences.getBool("isDarkMode");
+    if (sharedlang == "en") {
+      language = const Locale("en");
+    } else if (sharedlang == "ar") {
+      language = const Locale("ar");
+    } else {
+      String deviceLocale = Get.deviceLocale!.languageCode;
+      language = Locale(deviceLocale);
+    }
+    if (sharedisDarkMode == true) {
+      appTheme = ThemeData.dark(useMaterial3: true);
+    } else {
+      appTheme = ThemeData.light(useMaterial3: true);
+    }
+    super.onInit();
+  }
+}
