c0a01776370ccddc83ff425d1797df8e92ab93ce
Hisham Alyas <hishaam6618@gmail.com>
2024-06-12 06:59:09 +0200

    fix indentation in all_meals.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,15 @@ import 'package:get/get.dart';
 import 'package:food_delivery/const.dart';
 import 'package:food_delivery/auth/auth_page.dart';
 
-
-
-void main() async{
+void main() async {
   await WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-  SystemChrome.setEnabledSystemUIMode(
-      SystemUiMode.manual,
-      overlays:[]
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []
 
-    //[ // SystemUiOverlay.bottom,// SystemUiOverlay.top]
-  );
+      //[ // SystemUiOverlay.bottom,// SystemUiOverlay.top]
+      );
   runApp(const MyApp());
 }
 
@@ -28,35 +24,38 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return  ScreenUtilInit(
-        designSize:  Size(MediaQuery.of(context).size.width, MediaQuery.of(context).size.height,),
-    minTextAdapt: true,
-    splitScreenMode: true,
-    builder: (_, child) {
-    return GetMaterialApp(
-        navigatorKey: navigatorKey,
-        debugShowCheckedModeBanner: false,
-        defaultGlobalState: true,
-        title: 'Food',
-        theme: ThemeData(
-          // data: Theme.of(context).copyWith (
-          splashColor: Colors.transparent,
-          highlightColor: Colors.transparent,
-          hoverColor: Colors.transparent,
-          primaryColor: kprimaryColor,
+    return ScreenUtilInit(
+        designSize: Size(
+          MediaQuery.of(context).size.width,
+          MediaQuery.of(context).size.height,
         ),
-        locale: Get.deviceLocale,
-      home:AndroidAuthPage()
-      //WidgetTree()
-      //SignUpPage()
-      //TabBarTest()
-     // AndroidAuthPage()
-     // Process()
-      //SizesTest()
-     // AndroidAuthPage()
-      //CardStack()
-      //HomeScreen()
-    );});
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (_, child) {
+          return GetMaterialApp(
+              navigatorKey: navigatorKey,
+              debugShowCheckedModeBanner: false,
+              defaultGlobalState: true,
+              title: 'Food',
+              theme: ThemeData(
+                // data: Theme.of(context).copyWith (
+                splashColor: Colors.transparent,
+                highlightColor: Colors.transparent,
+                hoverColor: Colors.transparent,
+                primaryColor: kprimaryColor,
+              ),
+              locale: Get.deviceLocale,
+              home: AndroidAuthPage()
+              //WidgetTree()
+              //SignUpPage()
+              //TabBarTest()
+              //  AndroidAuthPage()
+              // Process()
+              //SizesTest()
+              // AndroidAuthPage()
+              //CardStack()
+              //HomeScreen()
+              );
+        });
   }
 }
-
